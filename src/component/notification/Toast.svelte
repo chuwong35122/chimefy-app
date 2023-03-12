bc2a40e149d4db0d80f49647dae8ba31f59b3a91
ChuChuwiiii <chuwong35122@gmail.com>
2023-03-12 09:59:41 +0700

    feat: add icons to Toast

--- src/component/notification/Toast.svelte
+++ src/component/notification/Toast.svelte
@@ -2,6 +2,7 @@
 	import { Toast } from 'flowbite-svelte';
 	import { fly } from 'svelte/transition';
 	import { toastValue, type ToastType } from '$lib/notification/toast';
+	import Icon from '@iconify/svelte';
 
 	const COUNTER_COUNTDOWN = 5;
 
@@ -52,19 +53,16 @@
 	divClass="p-2 w-full"
 >
 	<svelte:fragment slot="icon">
-		<svg
-			aria-hidden="true"
-			class="w-5 h-5"
-			fill="currentColor"
-			viewBox="0 0 20 20"
-			xmlns="http://www.w3.org/2000/svg"
-			><path
-				fill-rule="evenodd"
-				d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-				clip-rule="evenodd"
-			/></svg
-		>
-		<span class="sr-only">Error icon</span>
+	{#if $toastValue.type === 'error'}
+	<Icon icon='material-symbols:error' width={30} height={30} class='text-red-400' />
+	<span class="sr-only">Error icon</span>
+	{:else if $toastValue.type === 'warn'}
+	<Icon icon='material-symbols:error' width={30} height={30} class='text-amber-400' />
+	<span class="sr-only">Warn icon</span>
+	{:else if $toastValue.type === 'info'}
+	<Icon icon='material-symbols:info' width={30} height={30} class='text-blue-500'  />
+	<span class="sr-only">Info icon</span>
+	{/if}
 	</svelte:fragment>
 	<div>{$toastValue?.message}</div>
 </Toast>
