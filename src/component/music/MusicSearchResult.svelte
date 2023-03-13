a6b5895aae09db1e3d424b97085dda0351bb7904
ChuChuwiiii <chuwong35122@gmail.com>
2023-03-13 10:32:09 +0700

    feat: create track search bar UI

---
+++ src/component/music/MusicSearchResult.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<div class="p-2 my-1 hover:bg-gradient-to-r bg-black hover:from-[rgba(255,255,255,0.2)] to-[rgba(255,255,255,0.8)] duration-200 rounded-xl cursor-pointer">
+  <div class="flex flex-row items-center">
+    <div class="bg-[rgba(255,255,255,0.3)] grid place-items-center mr-4">
+      <img src='/logo/disc.png' alt='' width='80' height='80' />
+    </div>
+    <div>
+      <p class='font-medium'>Track name</p>
+      <p class='font-light text-xs'>Artist</p>
+    </div>
+  </div>
+  
+</div>
