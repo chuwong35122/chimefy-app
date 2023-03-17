92f8f2c600589657b1950b6e1d7af617ab5faabc
ChuChuwiiii <chuwong35122@gmail.com>
2023-03-17 22:01:53 +0700

    feat: add track queue

--- src/lib/session/session.ts
+++ src/lib/session/session.ts
@@ -1,10 +1,19 @@
-import type { MusicSession } from '$lib/interfaces/session.interface';
+import type { MusicSession, MusicSessionRole } from '$lib/interfaces/session.interface';
 import { toastValue } from '$lib/notification/toast';
 import { pb } from '$lib/pocketbase/pb';
 import type { ClientResponseError, Record } from 'pocketbase';
 import { writable } from 'svelte/store';
 
 export const currentSession = writable<Record & MusicSession>();
+
+export function checkSessionRole(
+	userId: string,
+	session: MusicSession
+): MusicSessionRole | undefined {
+	if (!userId || !session) return undefined;
+	const member = session?.participants?.find((participant) => participant.userId === userId);
+	return member?.role;
+}
 
 export async function getSessionData(id: string) {
 	if (!id) return;
