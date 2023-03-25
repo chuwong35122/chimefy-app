3fdca183ec47d70c170d992ab705fb9bea0890a8
ChuChuwiiii <chuwong35122@gmail.com>
2023-03-25 18:19:35 +0700

    feat: toggle play pause with Spotify web SDK

--- src/component/music/MusicPlayerController.svelte
+++ src/component/music/MusicPlayerController.svelte
@@ -3,11 +3,12 @@
 		currentSessionRole,
 		currentSession,
 		playingInfo,
-		SpotifyPlayer
+		SpotifyPlayer,
+		checkSessionRole
 	} from '$lib/session/session';
 	import Icon from '@iconify/svelte';
 	import { millisecondToMinuteSeconds } from '$lib/utils/common/time';
-	import { onMount } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 	import { socket } from '$lib/socket/client';
 	import type {
 		OnChangePlayingInfoRequest,
@@ -18,16 +19,12 @@
 	import { Tooltip } from 'flowbite-svelte';
 
 	let timer: NodeJS.Timer;
+	let debounceTimer: NodeJS.Timer;
 
-	let volume = 0;
-	let isPlaying = false;
+	let volume = 50;
+	let debouncedVolume = 0;
 	let playingMs = 0;
 
-	playingInfo.subscribe((val) => {
-		if (val) {
-			isPlaying = val.status === 'playing';
-		}
-	});
 	playingInfo.set({
 		trackName: 'Flowers',
 		artist: 'Miley Cyrus',
@@ -38,7 +35,17 @@
 		currentDurationMs: 0
 	});
 
+	async function debounceSetVolume() {
+		clearTimeout(debounceTimer);
+		debounceTimer = setTimeout(async () => {
+			debouncedVolume = volume / 100;
+			await $SpotifyPlayer.setVolume(debouncedVolume);
+		}, 300);
+	}
+
 	function handleChangeSessionInfo() {
+		if ($currentSessionRole !== 'admin') return;
+
 		const changePlayingInfoRequest: OnChangePlayingInfoRequest = {
 			playingInfo: $playingInfo,
 			sessionId: $currentSession?.id
@@ -47,11 +54,11 @@
 		socket.emit('onChangePlayingInfo', changePlayingInfoRequest);
 	}
 
-	function togglePlay() {
-		isPlaying = true;
-
+	async function togglePlay() {
 		const _playingInfo = $playingInfo;
 		_playingInfo.status = 'playing';
+		playingInfo.set(_playingInfo);
+		await $SpotifyPlayer?.togglePlay();
 		handleChangeSessionInfo();
 
 		timer = setInterval(() => {
@@ -71,10 +78,10 @@
 		}, 1000);
 	}
 
-	function togglePause() {
-		isPlaying = false;
+	async function togglePause() {
 		const _prevInfo = { ...$playingInfo };
 		playingInfo.set({ ..._prevInfo, status: 'pause' });
+		await $SpotifyPlayer?.togglePlay();
 		handleChangeSessionInfo();
 		clearInterval(timer);
 	}
@@ -90,17 +97,18 @@
 				},
 				volume: 0.5
 			});
-			player.connect();
 			SpotifyPlayer.set(player);
+			$SpotifyPlayer.connect();
 			$SpotifyPlayer.on('ready', () => {
 				toastValue.set({ message: 'Spotify Player is ready! 🎧', type: 'info' });
 			});
-			$SpotifyPlayer.on('authentication_error', (e) => {
-				console.log(e);
-			});
+			// $SpotifyPlayer.on('authentication_error', (e) => {
+			// 	console.log(e);
+			// });
 		};
 
 		socket.on('handleChangePlayingInfo', (info: SessionPlayingInfo) => {
+			if ($currentSessionRole === 'admin') return;
 			playingInfo.set(info);
 			if ($currentSessionRole === 'member') {
 				if (info.status === 'playing') {
@@ -111,88 +119,108 @@
 			}
 		});
 	});
+
+	onDestroy(() => {
+		$SpotifyPlayer?.disconnect();
+	});
 </script>
 
-<div class='relative w-full p-4 rounded-xl bg-dark-500 hover:bg-white/10 duration-200'>
-	<div
-	class="flex flex-row items-center justify-between"
->
-	{#if $playingInfo && $playingInfo?.trackCoverImg}
-		<div class="flex flex-col lg:flex-row items-center text-center lg:text-start">
-			<img
-				src={$playingInfo.trackCoverImg}
-				alt="Playing track cover"
-				class="w-16 h-16 rounded-full mr-4"
-			/>
-			<div class="w-32 overflow-hidden">
-				<p class="font-medium truncate w-28">{$playingInfo?.trackName ?? ''}</p>
-				<p class="text-xs truncate w-28 text-dark-200">{$playingInfo?.artist}</p>
+<svelte:head>
+	<title>Session - {$currentSession.name}</title>
+	<script src="https://sdk.scdn.co/spotify-player.js"></script>
+</svelte:head>
+<div>{$playingInfo.status}</div>
+<div class="relative w-full p-4 rounded-xl bg-dark-500 hover:bg-white/10 duration-200">
+	<div class="flex flex-row items-center justify-between">
+		{#if $playingInfo && $playingInfo?.trackCoverImg}
+			<div class="flex flex-col lg:flex-row items-center text-center lg:text-start">
+				<img
+					src={$playingInfo.trackCoverImg}
+					alt="Playing track cover"
+					class="w-16 h-16 rounded-full mr-4"
+				/>
+				<div class="w-32 overflow-hidden">
+					<p class="font-medium truncate w-28">{$playingInfo?.trackName ?? ''}</p>
+					<p class="text-xs truncate w-28 text-dark-200">{$playingInfo?.artist}</p>
+				</div>
+			</div>
+		{/if}
+
+		<div class="flex flex-col items-center">
+			<div class="flex flex-row items-center mb-2">
+				<button>
+					<Icon
+						icon="material-symbols:skip-previous-rounded"
+						class="w-10 h-10 hover:scale-110 duration-100"
+					/>
+				</button>
+				<button on:click={$playingInfo?.status === 'playing' ? togglePause : togglePlay}>
+					{#if $playingInfo && $playingInfo?.status !== 'playing'}
+						<Icon
+							icon="material-symbols:play-circle-rounded"
+							class="w-10 h-10 hover:scale-110 duration-100"
+						/>
+					{:else}
+						<Icon
+							icon="material-symbols:pause-circle-rounded"
+							class="w-10 h-10 hover:scale-110 duration-100"
+						/>
+					{/if}
+				</button>
+				<button>
+					<Icon
+						icon="material-symbols:skip-next-rounded"
+						class="w-10 h-10 hover:scale-110 duration-100"
+					/>
+				</button>
+			</div>
+			<div class="flex flex-row items-center w-full justify-between my-[-8px]">
+				<div class="text-xs hidden lg:block">{millisecondToMinuteSeconds(playingMs)}</div>
+				<input type="range" class="w-[500px] h-8 accent-primary-500 mx-2 hidden lg:block" />
+				<div class="text-xs hidden lg:block">
+					{millisecondToMinuteSeconds($playingInfo?.trackDurationMs ?? 0)}
+				</div>
 			</div>
 		</div>
-	{/if}
-
-	<div class="flex flex-col items-center">
-		<div class="flex flex-row items-center mb-2">
-			<button>
-				<Icon
-					icon="material-symbols:skip-previous-rounded"
-					class="w-10 h-10 hover:scale-110 duration-100"
+		<div class="flex flex-row items-center">
+			{#if volume === 0}
+				<Icon icon="material-symbols:volume-mute-rounded" class="w-6 h-6" />
+			{:else if volume > 0 && volume < 60}
+				<Icon icon="material-symbols:volume-down-rounded" class="w-6 h-6" />
+			{:else if volume >= 60}
+				<Icon icon="material-symbols:volume-up-rounded" class="w-6 h-6" />
+			{/if}
+			<div class="flex flex-col items-center justify-around">
+				<input
+					bind:value={volume}
+					on:keyup={debounceSetVolume}
+					type="range"
+					id="volume-sider"
+					class="accent-white w-16 md:w-24"
 				/>
-			</button>
-			<button on:click={isPlaying ? togglePause : togglePlay}>
-				{#if isPlaying}
-					<Icon
-						icon="material-symbols:pause-circle-rounded"
-						class="w-10 h-10 hover:scale-110 duration-100"
-					/>
-				{:else}
-					<Icon
-						icon="material-symbols:play-circle-rounded"
-						class="w-10 h-10 hover:scale-110 duration-100"
-					/>
-				{/if}
-			</button>
-			<button>
-				<Icon
-					icon="material-symbols:skip-next-rounded"
-					class="w-10 h-10 hover:scale-110 duration-100"
-				/>
-			</button>
+			</div>
 		</div>
-		<div class="flex flex-row items-center w-full justify-between my-[-8px]">
-			<div class="text-xs hidden lg:block">{millisecondToMinuteSeconds(playingMs)}</div>
-			<input type="range" class="w-[500px] h-8 accent-primary-500 mx-2 hidden lg:block" />
-			<div class="text-xs hidden lg:block">
+		<div class="absolute right-4 bottom-2">
+			<Icon
+				id="connected-player"
+				icon="material-symbols:speaker-group"
+				class="w-4 h-4 text-dark-200"
+			/>
+		</div>
+		<Tooltip triggeredBy="[id=connected-player]" placement="bottom"
+			>Connected to {$SpotifyPlayer?._options?.name}</Tooltip
+		>
+	</div>
+	<div class="grid place-items-center mt-4">
+		<div class="w-[300px] md:w-[560px] flex flex-row items-center justify-between">
+			<div class="text-xs lg:hidden block">{millisecondToMinuteSeconds(playingMs)}</div>
+			<div class="text-xs lg:hidden block">
 				{millisecondToMinuteSeconds($playingInfo?.trackDurationMs ?? 0)}
 			</div>
 		</div>
+		<input
+			type="range"
+			class="lg:hidden w-[300px] md:w-[560px] h-8 accent-primary-500 mx-2 block"
+		/>
 	</div>
-	<div class="flex flex-row items-center">
-		{#if volume === 0}
-			<Icon icon="material-symbols:volume-mute-rounded" class="w-6 h-6" />
-		{:else if volume > 0 && volume < 60}
-			<Icon icon="material-symbols:volume-down-rounded" class="w-6 h-6" />
-		{:else if volume >= 60}
-			<Icon icon="material-symbols:volume-up-rounded" class="w-6 h-6" />
-		{/if}
-		<div class="flex flex-col items-center justify-around">
-			<input bind:value={volume} type="range" id="volume-sider" class="accent-white w-16 md:w-24" />
-		</div>
-	</div>
-	<div class="absolute right-4 bottom-2">
-		<Icon id='connected-player' icon='material-symbols:speaker-group' class='w-4 h-4 text-dark-200' />
-	</div>
-	<Tooltip triggeredBy="[id=connected-player]" placement='bottom'>Connected to {$SpotifyPlayer?._options?.name}</Tooltip>
-	
 </div>
-<div class="grid place-items-center mt-4">
-	<div class="w-[300px] md:w-[560px] flex flex-row items-center justify-between">
-		<div class="text-xs lg:hidden block">{millisecondToMinuteSeconds(playingMs)}</div>
-		<div class="text-xs lg:hidden block">
-			{millisecondToMinuteSeconds($playingInfo?.trackDurationMs ?? 0)}
-		</div>
-	</div>
-	<input type="range" class="lg:hidden w-[300px] md:w-[560px] h-8 accent-primary-500 mx-2 block" />
-</div>
-</div>
-
