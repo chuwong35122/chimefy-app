bd91230896abe471ccb86f536aba8c29f77b6c84
ChuChuwiiii <chuwong35122@gmail.com>
2023-03-25 12:16:04 +0700

    feat: add responsive UI to member list

--- src/component/music/SessionQueueMembers.svelte
+++ src/component/music/SessionQueueMembers.svelte
@@ -14,11 +14,11 @@
 
 <div class="mt-20">
 	<h2 class="text-3xl font-bold">Session Members</h2>
-	<div class="w-full p-4 rounded-xl bg-black grid grid-cols-4 gap-4">
+	<div class="w-full p-4 rounded-xl bg-black grid grid-cols-3 lg:grid-cols-4 gap-2 lg:gap-4">
 		{#each participants as member}
 			<div class="grid place-items-center relative group">
 				<div
-					class="w-32 h-32 rounded-full absolute top-0 bg-gradient-to-b from-dark-400/40 to-white/30 grid place-items-center hover:from-dark-400/0 hover:to-white/0 duration-300"
+					class="w-24 h-24 lg:w-32 lg:h-32 rounded-full absolute top-0 bg-gradient-to-b from-dark-400/40 to-white/30 grid place-items-center hover:from-dark-400/0 hover:to-white/0 duration-300"
 				>
 					{#if member.role === 'admin'}
 						<Icon
@@ -36,9 +36,9 @@
 					src={member?.profileImg}
 					alt={`${member?.spotifyDisplayedName} profile`}
 					draggable="false"
-					class="w-32 h-32 rounded-full"
+					class="w-24 h-24 lg:w-32 lg:h-32 rounded-full"
 				/>
-				<p class="mt-2">{member?.spotifyDisplayedName}</p>
+				<p class="mt-2 text-sm lg:text-md">{member?.spotifyDisplayedName}</p>
 			</div>
 		{/each}
 	</div>
