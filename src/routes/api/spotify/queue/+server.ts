fab85fc56ffe004e628a6719b1ed88529bcf1070
ChuChuwiiii <chuwong35122@gmail.com>
2023-03-26 18:29:59 +0700

    feat: endpoint for add queue to Spotify

---
+++ src/routes/api/spotify/queue/+server.ts
@@ -0,0 +1,23 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { PUBLIC_SPOTIFY_BASE_URL } from '$env/static/public';
+import { getBearerToken } from '$lib/spotify/spotify';
+
+/**
+ * Add queue to Spotify queue (body: access_token, uri, device_id)
+ */
+export const POST: RequestHandler = async ({ fetch, request }) => {
+	const { access_token, uri, device_id } = await request.json();
+
+	const addQueueRes = await fetch(`${PUBLIC_SPOTIFY_BASE_URL}/me/player/queue`, {
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: getBearerToken(access_token ?? '')
+		},
+		body: JSON.stringify({
+			uri,
+			device_id
+		})
+	});
+	return json(await addQueueRes.json());
+};
