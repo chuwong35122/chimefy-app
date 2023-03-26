6b14f36412cc1619033ae72c4cffa9ce30b6d7be
ChuChuwiiii <chuwong35122@gmail.com>
2023-03-26 18:21:29 +0700

    feat: remove queue from Pocketbase

--- src/component/music/TrackQueueList.svelte
+++ src/component/music/TrackQueueList.svelte
@@ -4,6 +4,11 @@
 	import { millisecondToMinuteSeconds, minuteSecondsToTime } from '$lib/utils/common/time';
 	import { onDestroy, onMount } from 'svelte';
 	import { toastValue } from '$lib/notification/toast';
+	import Icon from '@iconify/svelte';
+	import { Tooltip } from 'flowbite-svelte';
+	import type { MusicSession, MusicSessionQueue } from '$lib/interfaces/session/session.interface';
+	import { pb } from '$lib/pocketbase/pb';
+	import type {Record} from 'pocketbase'
 	// TODO: Create scroll animation
 	let imgRef: HTMLImageElement;
 
@@ -11,12 +16,29 @@
 		imgRef.src = '/logo/disc.png';
 	}
 
+	async function removeQueue(index: number) {
+		if(!$currentSession || !$currentSession?.id) return;
+
+		const _currentSession = $currentSession;
+		const queues = _currentSession.queues
+		queues.splice(index, 1)
+		await pb.collection('sessions').update($currentSession?.id, _currentSession)
+	}
+	
+
 	onMount(() => {
+		if (!$currentSession || !$currentSession?.id) return;
+
+		pb.collection('sessions').subscribe($currentSession?.id, async () => {
+			const session = await pb.collection('sessions').getOne<MusicSession & Record>($currentSession?.id);
+			currentSession.set(session);
+		});
+		
 		socket.on('onQueueAdded', (message: string) => {
 			toastValue.set({message: message, type: 'info'})
 		})
 	})
-	
+
 	onDestroy(() => {
 		socket.off('onQueueAdded')
 	})
@@ -24,7 +46,7 @@
 
 <!-- Each Queues -->
 <div class="w-full grid place-items-center">
-	{#each $currentSession?.queues as queue}
+	{#each $currentSession?.queues as queue, i}
 			<div class="w-60 h-60 lg:w-96 lg:h-96 rounded-2xl overflow-hidden relative cursor-pointer my-8">
 				<div class="gradient-hover-effect" />
 				<img
@@ -43,8 +65,12 @@
 					<p class="text-2xl font-normal leading-tight">{queue?.trackName}</p>
 					<p class="text-sm leading-tight">{queue?.artist}</p>
 				</div>
+				<button on:click={() => removeQueue(i)} id='delete-btn' class="absolute z-20 p-2 top-0 right-0">
+					<Icon icon='mdi:trash' class='w-6 h-6 text-dark-200 hover:scale-110 duration-200' />
+				</button>
 			</div>
 	{/each}
+	<Tooltip triggeredBy="[id=delete-btn]" placement="right">Remove Queue</Tooltip>
 	<!-- Each Queues -->
 </div>
 
