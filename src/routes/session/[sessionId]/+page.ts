0d651a5cece0674837fa3c7ab4bab9216ad6ee8d
ChuChuwiiii <chuwong35122@gmail.com>
2023-03-26 18:23:14 +0700

    feat: return session not found if there's no session

--- src/routes/session/[sessionId]/+page.ts
+++ src/routes/session/[sessionId]/+page.ts
@@ -1,11 +1,19 @@
 import type { MusicSession } from '$lib/interfaces/session/session.interface';
 import { pb } from '$lib/pocketbase/pb';
 import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
 export const load: PageLoad = async ({ params }) => {
-	const data = await pb.collection('sessions').getOne<MusicSession>(params.sessionId);
+	try {
+		const data = await pb.collection('sessions').getOne<MusicSession>(params.sessionId);
+		return {
+			session: data
+		};
+	} catch (e) {
+		throw error(404, {
+			message: 'Session not found.'
+		});
+	}
+	
 
-	return {
-		session: data
-	};
 };
