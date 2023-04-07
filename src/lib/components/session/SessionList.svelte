b777d74bd20b738cacddaa6f5e52c3adf44c1600
ChuChuwiiii <chuwong35122@gmail.com>
2023-04-07 20:05:32 +0700

    feat: update session search filter UI

--- src/lib/components/session/SessionList.svelte
+++ src/lib/components/session/SessionList.svelte
@@ -13,61 +13,66 @@
 	}
 </script>
 
-<div class="grid place-items-center mb-4 w-96">
-	<h2 class="text-2xl font-semibold">Search for Session</h2>
+<div class="grid place-items-center mb-4">
+	<h2 class="text-2xl font-semibold">Public Radio</h2>
 </div>
 
 <SessionSearchFilter />
 
-<div class="h-[400px] overflow-y-auto">
+<div class="h-[400px] w-[400px] md:w-[600px] lg:w-[800px] overflow-y-auto">
 	{#if $sessionSearchResult.loading}
-	<div class="w-full grid place-items-center">
-		<Spinner color='green' />
-	</div>
-	{:else}
-	{#if $sessionSearchResult.results.length > 0}
-	<!-- Session Item -->
-	{#each $sessionSearchResult.results as session}
-		<div
-			on:mouseup={() => handleSessionNavigate(session)}
-			class="w-[300px] lg:w-[600px] h-28 p-4 rounded-xl bg-dark-500 hover:bg-dark-400/20 duration-150 my-2 relative hover:cursor-pointer"
-		>
-			<div class="flex items-center">
-				<p class="mr-2">{session?.name}</p>
-				<Chip label={session?.type} />
-			</div>
-			{#if session?.queues && session?.queues[0]}
-				<div class="mt-6">
-					<p class="font-light text-xs text-dark-200">Now Listening</p>
-					<p class="font-light text-xs w-80 text-ellipsis">
-						{session?.queues[0]?.trackName} - {session?.queues[0]?.artist}
-					</p>
-				</div>
-			{/if}
-
-			<div class="absolute bottom-2 right-0 z-10">
-				<div class="flex h-10">
-					<!-- Participant List -->
-					{#each session.participants as participant, i}
-						<img
-							src={participant?.profileImg}
-							draggable="false"
-							alt={`Participant photo ${i + 1}`}
-							class={`w-8 h-8 rounded-full ${i > 0 ? 'ml-[-20px] z-[i+1]' : 'ml-0 z-0'}`}
-						/>
-					{/each}
-				</div>
-				<Icon icon="material-symbols:chevron-right-rounded" class="w-12 h-12 text-dark-400" />
-			</div>
+		<div class="w-full grid place-items-center">
+			<Spinner color="green" />
 		</div>
-	{/each}
-	<!-- Session Item -->
-{:else}
+	{:else if $sessionSearchResult.results.length > 0}
+		<!-- Session Item -->
+		{#each $sessionSearchResult.results as session}
+			<div
+				on:mouseup={() => handleSessionNavigate(session)}
+				class="w-[400px] md:w-[600px] lg:w-[800px] p-4 rounded-xl bg-dark-500 hover:bg-dark-300/20 duration-150 my-2 relative hover:cursor-pointer"
+			>
+				<div class="flex items-center justify-between">
+					<p class="mr-2 capitalize text-lg font-medium">{session?.name}</p>
+					<Chip label={session?.type} />
+				</div>
+				<div class="flex items-end mt-2">
+					<img
+						src={session?.queues[0]?.trackImageUrl}
+						alt="Playing track cover"
+						class="w-20 h-20 rounded-full mr-4"
+					/>
+					{#if session?.queues && session?.queues[0]}
+						<div class='flex flex-items flex-col lg:flex-row'>
+							<p class="font-light text-xs text-dark-200 mr-2 uppercase">Now Playing</p>
+							<p class="font-light text-xs w-80 text-ellipsis">
+								{session?.queues[0]?.trackName} - {session?.queues[0]?.artist}
+							</p>
+						</div>
+					{/if}
+				</div>
+
+				<div class="absolute bottom-2 right-0 z-10">
+					<div class="flex h-10">
+						<!-- Participant List -->
+						{#each session.participants as participant, i}
+							<img
+								src={participant?.profileImg}
+								draggable="false"
+								alt={`Participant photo ${i + 1}`}
+								class={`w-8 h-8 rounded-full ${i > 0 ? 'ml-[-20px] z-[i+1]' : 'ml-0 z-0'}`}
+							/>
+						{/each}
+					</div>
+					<Icon icon="material-symbols:chevron-right-rounded" class="w-12 h-12 text-dark-300" />
+				</div>
+			</div>
+		{/each}
+		<!-- Session Item -->
+	{:else}
 		<div class="w-full rounded-xl p-4 bg-dark-500 grid place-items-center">
 			<p class="text-xl text-medium">Sorry! No session found!</p>
-			<Icon icon='ri:emotion-sad-line' class='w-16 h-16 my-2' />
-			<p class='text-sm text-dark-200'>Tips: Try searching for other things!</p>
+			<Icon icon="ri:emotion-sad-line" class="w-16 h-16 my-2" />
+			<p class="text-sm text-dark-200">Tips: Try searching for other things!</p>
 		</div>
-{/if}
 	{/if}
 </div>
