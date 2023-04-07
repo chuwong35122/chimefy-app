1a206ad89dc90d3bc9c91a5c09c2eba950cf114f
ChuChuwiiii <chuwong35122@gmail.com>
2023-04-07 20:05:32 +0700

    feat: update session search filter UI

--- src/lib/components/session/SessionSearchFilter.svelte
+++ src/lib/components/session/SessionSearchFilter.svelte
@@ -1,123 +1,53 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
-	import { Tooltip } from 'flowbite-svelte';
-	import PrimaryButton from '../buttons/PrimaryButton.svelte';
-	import TextField from '../UI/TextField.svelte';
-	import { sessionSearchResult } from '$lib/session/search';
-	import { pb } from '$lib/pocketbase/pb';
-	import type { MusicSession } from '$lib/interfaces/session/session.interface';
-	import type { Record } from 'pocketbase';
+	import { Search, Tooltip } from 'flowbite-svelte';
 	import { onMount } from 'svelte';
+	import {getSessionList} from '$lib/session/search';
 
+	let timer: NodeJS.Timeout;
 	export let queryInput = {
 		sessionName: '',
 		musicType: ''
 	};
 
-	async function getSessionList() {
-		sessionSearchResult.set({
-			results: [],
-			loading: true
-		});
-		const searchTerms = ['isPrivate = false'];
-
-		if (queryInput.sessionName) {
-			searchTerms.push(`sessionName = ${queryInput.sessionName}`);
-		}
-
-		if (queryInput.musicType) {
-			searchTerms.push(`musicType = ${queryInput.musicType}`);
-		}
-
-		try {
-			const lists = await pb.collection('sessions').getList<MusicSession & Record>(1, 20, {
-				filter: searchTerms.join(' && ')
-			});
-
-			sessionSearchResult.set({
-				results: lists.items,
-				loading: true
-			});
-		} catch (e) {
-		} finally {
-			sessionSearchResult.set({
-				results: $sessionSearchResult.results,
-				loading: false
-			});
-		}
+	let debouncedQueryInput = {
+		sessionName: '',
+		musicType: ''
 	}
 
-	async function handleRefreshSessionList() {
+	async function debounceQueryInput() {
+		clearTimeout(timer);
+		timer = setTimeout(async () => {
+			debouncedQueryInput = queryInput;
+			const { sessionName, musicType } = debouncedQueryInput;
+			await getSessionList(sessionName, musicType, 1);
+		}, 500);
+	}
+
+	async function refreshList() {
+		await getSessionList('', '', 1);
 		queryInput = {
 			sessionName: '',
 			musicType: ''
-		};
-		await getSessionList();
+		}
+	}
+
+	$: if(queryInput.sessionName) {
+		debounceQueryInput();
 	}
 
 	onMount(async () => {
-		await getSessionList();
+		const { sessionName, musicType } = queryInput;
+		await getSessionList(sessionName, musicType, 1);
 	});
 </script>
 
-<Tooltip triggeredBy="[id=search-list-btn]" placement="bottom">Search</Tooltip>
-<Tooltip triggeredBy="[id=refresh-btn]" placement="bottom">Refresh this list</Tooltip>
-<div class="rounded-xl w-full bg-dark-500 p-4 hover:bg-white/10 duration-200 mt-2 mb-8 relative">
-	<div class="flex flex-row justify-between items-center w-full">
-		<h3 class="text-lg font-medium">Filter</h3>
-		<Icon
-			id="info-icon"
-			icon="material-symbols:info-rounded"
-			class="w-4 h-4 text-dark-100/75 hover:text-dark-50/50 duration-200 mb-1"
-		/>
-	</div>
-	<Tooltip triggeredBy="[id=info-icon]">Private session will not show here</Tooltip>
-	<div class="flex sm:flex-col lg:flex-row justify-between items-center gap-4 w-full">
-		<TextField
-			bind:value={queryInput.sessionName}
-			placeholder="Session Name"
-			required
-			class="w-80 lg:w-64 rounded-md"
-			labelClass="text-white"
-		/>
-		<TextField
-			bind:value={queryInput.musicType}
-			placeholder="Music Type (Chill/ Hiphop)"
-			required
-			class="placeholder:text-sm w-80 lg:w-56 rounded-md"
-			labelClass="text-white"
-		/>
-		<button
-			on:click|preventDefault={getSessionList}
-			id="search-list-btn"
-			class="hidden lg:grid place-items-center bg-white hover:bg-white/60 duration-200 rounded-lg p-1 active:scale-105"
-		>
-			<Icon icon="material-symbols:search" class="w-8 h-8 text-black" />
-		</button>
+<Tooltip triggeredBy="[id=refresh-btn]" placement="top">Refresh this list</Tooltip>
 
-		<PrimaryButton
-			class="!w-80 lg:hidden active:scale-105 mr-2 bg-primary-600 hover:bg-primary-400 duration-200 p-2 rounded-full font-regular"
-		>
-			<button on:click|preventDefault={getSessionList} class='w-full items-center'>Search</button></PrimaryButton
-		>
-		<PrimaryButton
-			class="!w-80 lg:hidden active:scale-105 mr-2 border border-dark-200 bg-transparent hover:bg-white/10"
-		>
-			<div
-				on:mousedown={handleRefreshSessionList}
-				class="flex flex-row items-center justify-center"
-			>
-				<Icon icon="material-symbols:refresh-rounded" class="w-6 h-6" />
-				<span>Refresh</span>
-			</div>
-		</PrimaryButton>
-		<button
-			on:click={handleRefreshSessionList}
-			id="refresh-btn"
-			class="hidden lg:grid place-items-center bg-white hover:bg-white/60 duration-200 rounded-lg p-1 active:scale-105"
-		>
-			<Icon icon="material-symbols:refresh-rounded" class="w-8 h-8 text-black" />
-		</button>
-	</div>
-	<div class="w-full grid place-items-center my-2" />
-</div>
+	<form class="w-full flex gap-2">
+		<Search bind:value={queryInput.sessionName} size='md' class="flex gap-2 items-center" placeholder="Search Session Name" defaultClass='w-full focus:border-primary-500 !rounded-full'>
+			<button id='refresh-btn' type='button' on:click={refreshList} class='rounded-lg hover:scale-105 duration-150'>
+				<Icon icon='material-symbols:refresh-rounded' class='w-8 h-8 text-black' />
+			</button>
+		</Search>
+	</form>
