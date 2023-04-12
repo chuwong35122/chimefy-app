e22459d8ab14ffaf513e62a70a8ecbd64abec98d
ChuChuwiiii <chuwong35122@gmail.com>
2023-04-12 14:28:27 +0700

    fix: auto play/pause track

--- src/routes/api/spotify/playback/play/+server.ts
+++ src/routes/api/spotify/playback/play/+server.ts
@@ -1,4 +1,3 @@
-import { json } from '@sveltejs/kit';
 import type { RequestHandler } from '../$types';
 import { PUBLIC_SPOTIFY_BASE_URL } from '$env/static/public';
 import { getBearerToken } from '$lib/spotify/spotify';
@@ -6,9 +5,9 @@ import { getBearerToken } from '$lib/spotify/spotify';
 // Play each Spotify track base on its position (ms)
 // uwis is in [spotify:track, ...] format
 export const POST: RequestHandler = async ({ fetch, request }) => {
-	const req = await request.json();
-	console.log(req);
-	const { access_token, device_id, uris, position_ms } = req;
+	const req = request.clone();
+	const _req = await req.json();
+	const { access_token, device_id, uris, position_ms } = _req;
 
 	const res = await fetch(`${PUBLIC_SPOTIFY_BASE_URL}/me/player/play`, {
 		headers: {
@@ -23,8 +22,5 @@ export const POST: RequestHandler = async ({ fetch, request }) => {
 		})
 	});
 
-	console.log(await res.json());
-	return json({
-		uwu: 'owo'
-	});
+	return await res.json();
 };
