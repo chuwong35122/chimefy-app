d0d4e6fc2de7266aef3f2074a9de542b91b50f0c
ChuChuwiiii <chuwong35122@gmail.com>
2023-04-12 23:20:44 +0700

    fix: modal imports

--- src/lib/components/modals/SpotifyTrackBroadcastModal.svelte
+++ src/lib/components/modals/SpotifyTrackBroadcastModal.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { Modal } from 'flowbite-svelte';
 	import PrimaryButton from '../buttons/PrimaryButton.svelte';
 	import Icon from '@iconify/svelte';
 	import { pb } from '$lib/pocketbase/pb';
@@ -10,7 +9,6 @@
 	import { spotifyAccessToken } from '$lib/spotify/spotify';
 	import { spotifyPlayerDeviceId } from '$lib/session/session';
 
-	export let open = false;
 	const dispatch = createEventDispatcher();
 
 	async function onSessionBroadcast() {
@@ -33,20 +31,18 @@
 	}
 </script>
 
-<Modal bind:open size="xs" autoclose={false} class="bg-dark-600">
-	<div class="text-center w-80 grid place-items-center">
-		<Icon icon="mdi:broadcast" class="w-16 h-16 text-primary-500" />
-		<h1 class="text-lg font-normal text-white">Start Session Broadcast?</h1>
-		<div class="w-72 mb-4">
-			<p class="text-xs text-dark-100 mt-2">
-				By pressing this button, you will play the music and all of your music will be sync to every
-				session members
-			</p>
-		</div>
-		<button on:click={onSessionBroadcast}>
-			<PrimaryButton>
-				<div class="px-2">Start Broadcast</div>
-			</PrimaryButton>
-		</button>
+<div class="text-center w-80 grid place-items-center">
+	<Icon icon="mdi:broadcast" class="w-16 h-16 text-primary-500" />
+	<h1 class="text-lg font-normal text-white">Start Session Broadcast?</h1>
+	<div class="w-72 mb-4">
+		<p class="text-xs text-dark-100 mt-2">
+			By pressing this button, you will play the music and all of your music will be sync to every
+			session members
+		</p>
 	</div>
-</Modal>
+	<button on:click={onSessionBroadcast}>
+		<PrimaryButton>
+			<div class="px-2">Start Broadcast</div>
+		</PrimaryButton>
+	</button>
+</div>
