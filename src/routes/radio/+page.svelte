a2cdc7b8da37615780f33079f8d650c54eec28a0
ChuChuwiiii <chuwong35122@gmail.com>
2023-04-15 12:05:39 +0700

    feat: add HTML title to pages

--- src/routes/radio/+page.svelte
+++ src/routes/radio/+page.svelte
@@ -4,6 +4,9 @@
 
 </script>
 
+<svelte:head>
+  <title>Radio is Coming Soon!</title>
+</svelte:head>
 <Alert color="dark">
   <span class="font-medium">Hang Tight!</span> This feature is coming soon! 🚧
 </Alert>
