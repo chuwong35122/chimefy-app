8870300af2c73347fc0f836a86acb19feb3d98f3
ChuChuwiiii <chuwong35122@gmail.com>
2023-04-15 14:03:46 +0700

    chore: rename file to wrapper

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import { Button, ButtonGroup, Input } from 'flowbite-svelte';
-	import PrimaryButton from '$lib/components/buttons/PrimaryButton.svelte';
+	import PrimaryButtonWrapper from '$lib/components/buttons/PrimaryButtonWrapper.svelte';
 	import { toastValue } from '$lib/notification/toast';
 	import type { ActionData } from './$types';
 	import Icon from '@iconify/svelte';
@@ -65,7 +65,11 @@
 				/>
 			</Button>
 		</ButtonGroup>
-		<PrimaryButton class="my-2">Login</PrimaryButton>
+		<PrimaryButtonWrapper class="my-2">
+			<button>
+				Login
+			</button>
+		</PrimaryButtonWrapper>
 		<a href="/auth/register" class="underline text-gray-300 text-sm">Create an account</a>
 	</form>
 </div>
