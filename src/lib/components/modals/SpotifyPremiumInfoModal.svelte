db43281ff7960d7f5fd4e9c1d144af6db02d14c3
ChuChuwiiii <chuwong35122@gmail.com>
2023-04-15 14:02:41 +0700

    feat: open modal if account is not Premium

---
+++ src/lib/components/modals/SpotifyPremiumInfoModal.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { spotifyUser } from '$lib/spotify/spotify';
+	import { createEventDispatcher } from 'svelte';
+	import SecondaryButton from '../buttons/SecondaryButton.svelte';
+
+	const dispatcher = createEventDispatcher();
+	function handleLogout() {
+		dispatcher('logout');
+	}
+</script>
+
+<div class="w-full text-white">
+	<h1 class="text-xl font-semibold mb-4">Hold Up!</h1>
+	<h2 class="text-sm">
+		Chimefy only works with <span class="font-semibold text-primary-500"
+			>Premium Spotify Account</span
+		>!
+	</h2>
+	<p class="text-sm">
+		Your current Spotify plan is: <span class="capitalize font-bold">{$spotifyUser?.product}</span>
+	</p>
+  <div class='w-full h-4' />
+	<p class="text-sm">Please logout and login with a Premium Spotify account.</p>
+	<button class='w-full my-4'>
+		<SecondaryButton>Logout</SecondaryButton>
+	</button>
+		<a href="https://www.spotify.com/us/premium/" class="text-xs underline underline-offset-1"
+			>More about Spotify Premium</a
+		>
+</div>
