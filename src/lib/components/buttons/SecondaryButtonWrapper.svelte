cfd83f740329010c2d5f48a505d28a0225890982
ChuChuwiiii <chuwong35122@gmail.com>
2023-04-16 12:58:56 +0700

    feat: add modal informing non-premium user

--- src/lib/components/buttons/SecondaryButtonWrapper.svelte
+++ src/lib/components/buttons/SecondaryButtonWrapper.svelte
@@ -6,7 +6,7 @@
 
 <div
 	{...props}
-	class={`w-full text-lg border border-white px-4 py-2 rounded-full hover:scale-105 hover:bg-[rgba(255,255,255,0.2)] duration-200 ${props.class}`}
+	class={`w-full text-[16px] border border-white py-1.5 rounded-full hover:scale-105 bg-dark-500 px-2 hover:bg-[rgba(255,255,255,0.2)] duration-200 ${props.class}`}
 >
 	<slot />
 </div>
