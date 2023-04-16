cac59f88a956a465b73c1a7d90ad44e529643997
ChuChuwiiii <chuwong35122@gmail.com>
2023-04-16 12:03:39 +0700

    feat: add title to all pages

---
+++ src/routes/radio/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+
+	import { Alert } from "flowbite-svelte";
+
+</script>
+
+<svelte:head>
+  <title>Radio is Coming Soon!</title>
+</svelte:head>
+<Alert color="dark">
+  <span class="font-medium">Hang Tight!</span> This feature is coming soon! 🚧
+</Alert>
