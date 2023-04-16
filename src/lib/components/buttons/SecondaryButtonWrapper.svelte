a3ec9b1e32271fbd2e9b7e3a5a6f92916d89ee98
ChuChuwiiii <chuwong35122@gmail.com>
2023-04-16 12:51:32 +0700

    feat: rename secondary button to wrapper

--- src/lib/components/buttons/SecondaryButtonWrapper.svelte
+++ src/lib/components/buttons/SecondaryButtonWrapper.svelte
@@ -4,10 +4,9 @@
 	};
 </script>
 
-<button
-	on:click
+<div
 	{...props}
 	class={`w-full text-lg border border-white px-4 py-2 rounded-full hover:scale-105 hover:bg-[rgba(255,255,255,0.2)] duration-200 ${props.class}`}
 >
 	<slot />
-</button>
+</div>
