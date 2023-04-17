c89181e2444bd18eea4f0cb0f4024052e78184b6
ChuChuwiiii <chuwong35122@gmail.com>
2023-04-17 11:53:51 +0700

    wip: show initialization modal

---
+++ src/lib/constants/session.ts
@@ -0,0 +1,45 @@
+export const ADMIN_SESSION_INTIALIZE_PROCESS = [
+	{
+		key: 0,
+		step: 'session_init',
+		description: 'Starting Session Initialization Process 🏁'
+	},
+	{
+		key: 1,
+		step: 'connecting_player',
+		description: 'Connecting to Spotify Player 🎵'
+	},
+	{
+		key: 2,
+		step: 'player_connected',
+		description: 'Spotify Player connected! 🎉'
+	},
+	{
+		key: 3,
+		step: 'socket_started',
+		description: 'Socket Connection Started 🔌'
+	}
+];
+
+export const MEMBER_SESSION_INTIALIZE_PROCESS = [
+	{
+		key: 0,
+		step: 'session_init',
+		description: 'Starting Session Initialization Process 🏁'
+	},
+	{
+		key: 1,
+		step: 'socket_start',
+		description: 'Starting Socket Connection 🔌'
+	},
+	{
+		key: 2,
+		step: 'connecting_player',
+		description: 'Connecting to Spotify Player 🎵'
+	},
+	{
+		key: 3,
+		step: 'player_connected',
+		description: 'Spotify Player connected! 🎉'
+	}
+];
--- src/routes/session/[sessionId]/+page.svelte
+++ src/routes/session/[sessionId]/+page.svelte
@@ -14,14 +14,21 @@
 		checkSessionRole,
 		currentSession,
 		currentSessionRole,
+		increamentInitializationProcess,
 		playingInfo,
-		socketId
+		adminSessionInitProcess,
+		socketId,
+		sessionInitModalOpen,
+		memberSessionInitProcess
 	} from '$lib/session/session';
 	import TrackQueueList from '$lib/components/music/TrackQueueList.svelte';
 	import { spotifyAccessToken, spotifyUser } from '$lib/spotify/spotify';
 	import MusicPlayerController from '$lib/components/music/MusicPlayerController.svelte';
 	import SessionQueueMembers from '$lib/components/music/SessionQueueMembers.svelte';
 	import SessionInfo from '$lib/components/music/SessionInfo.svelte';
+	import { Modal } from 'flowbite-svelte';
+	import SessionInitializeProcessModal from '$lib/components/modals/SessionInitializeProcessModal.svelte';
+	import { wait } from '$lib/utils/common/time';
 
 	// TODO: Store session password and check before entering
 	export let data: { session: MusicSession & Record };
@@ -32,6 +39,28 @@
 
 	let sessionId = data.session.id;
 
+	adminSessionInitProcess.subscribe((val) => {
+		if($currentSessionRole !== 'admin') return
+
+		if(val === 3) {
+			setTimeout(() => {
+				sessionInitModalOpen.set(false)
+			}, 1000)
+		}
+		else sessionInitModalOpen.set(true)
+	})
+
+	memberSessionInitProcess.subscribe((val) => {
+		if($currentSessionRole !== 'member') return
+
+		if(val === 4) {
+			setTimeout(() => {
+				sessionInitModalOpen.set(false)
+			}, 1000)
+		}
+		else sessionInitModalOpen.set(true)
+	})
+
 	onMount(async () => {
 		// TODO: add hashed password in query params
 
@@ -41,11 +70,14 @@
 		// 	goto('/session');
 		// 	toastValue.set({ message: "You need session's password", type: 'warn' });
 		// }
+		await wait(2000)
 		if (!sessionId || !$user || !$user?.id || !$spotifyUser) return;
-
+	
 		const socketConnection = socket.connect();
-
+		console.log('3')
+		increamentInitializationProcess($currentSessionRole)
 		// Admin: send current playing track to new comers
+		
 		const joinSessionRequest: SessionJoinRequest = {
 			sessionId: sessionId,
 			socketId: socketConnection.id,
@@ -53,17 +85,20 @@
 			spotifyDisplayName: $spotifyUser.display_name ?? '',
 			playingInfo: $playingInfo
 		};
-
-		// Connect to Music session and request for its states
-		socket.on('connect', () => {
+		
+		// Connect to Music session and request for its states (not participants)
+		socket.on('connect', async () => {
 			socketId.set(socketConnection.id);
 			socket.emit('joinSession', joinSessionRequest);
+			console.log('4')
+			increamentInitializationProcess($currentSessionRole)
 		});
 
 		socket.on('onNewComerJoin', (joinResponse: SessionJoinResponse) => {
 			toastValue.set({ message: `Say hi to ${joinResponse.spotifyDisplayName} 👋`, type: 'info' });
 			const sessionRole = checkSessionRole($user?.id, $currentSession);
 			currentSessionRole.set(sessionRole);
+
 
 			if (sessionRole === 'member') {
 				playingInfo.set(joinResponse.playingInfo);
@@ -88,6 +123,10 @@
 <svelte:head>
 	<title>Listening to {$currentSession.name}</title>
 </svelte:head>
+
+<Modal open={$sessionInitModalOpen} size="lg" class="modal-glass">
+	<SessionInitializeProcessModal />
+</Modal>
 <div class="p-4 w-[400px] md:w-[640px] lg:w-[1000px]">
 	<SessionInfo {sessionId} />
 </div>
