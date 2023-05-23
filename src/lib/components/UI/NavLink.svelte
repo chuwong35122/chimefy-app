f1378cd9ad764bd4889701f92f2f99b8abeedc82
ChuChuwiiii <chuwong35122@gmail.com>
2023-05-23 14:52:56 +0700

    feat: show forbidden route indicator in Navbar for unauthorized user

---
+++ src/lib/components/UI/NavLink.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Icon from '@iconify/svelte';
+
+	export let icon: string = 'material-symbols:question-mark-rounded';
+	export let route: `/${string}` = '/';
+	export let routeName: string = '';
+	export let isActive = true;
+</script>
+
+{#if isActive}
+	<a
+		href={route}
+		class={`flex flex-row items-center p-2 px-3 duration-200 hover:text-white ${
+			$page.route.id === route
+				? 'text-white bg-[rgba(255,255,255,0.08)] rounded-lg'
+				: 'text-gray-400'
+		}`}
+	>
+		<Icon {icon} width={24} height={24} class="mb-1" />
+		<p class="text-lg font-semibold h-full duration-200 ml-2 hover:text-white hidden md:block">
+			{routeName}
+		</p>
+	</a>
+{:else}
+	<button disabled class={`flex flex-row items-center p-2 px-3 text-dark-400 ${!isActive ? 'cursor-not-allowed' : null}`}>
+		<Icon {icon} width={24} height={24} class="mb-1" />
+		<p class="text-lg font-semibold h-full ml-2 hidden md:block">
+			{routeName}
+		</p>
+	</button>
+{/if}
