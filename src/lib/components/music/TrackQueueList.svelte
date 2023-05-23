e33b0c89046f4d1243c9153e8cd066e862c7d6b5
ChuChuwiiii <chuwong35122@gmail.com>
2023-05-23 16:00:40 +0700

    feat: simplify queue list UI

--- src/lib/components/music/TrackQueueList.svelte
+++ src/lib/components/music/TrackQueueList.svelte
@@ -2,7 +2,7 @@
 	import { currentSessionQueue, currentSession } from '$lib/session/session';
 	import { millisecondToMinuteSeconds } from '$lib/utils/common/time';
 	import Icon from '@iconify/svelte';
-import { Tooltip } from 'flowbite-svelte';
+	import { Tooltip } from 'flowbite-svelte';
 	import type { MusicQueue } from '$lib/interfaces/session/queue.interface';
 	import { supabase } from '$lib/supabase/supabase';
 	// TODO: Create scroll animation
@@ -13,71 +13,57 @@ import { Tooltip } from 'flowbite-svelte';
 	}
 
 	let queues: MusicQueue[] = [];
+	console.log(queues);
 	currentSessionQueue.subscribe((val) => {
-		if (val) queues = val?.queues;
+		if (val && val?.queues) {
+			queues = val?.queues ?? [];
+		}
 	});
 
 	async function removeQueue(index: number) {
 		if (!$currentSession || !$currentSessionQueue) return;
 
 		const queues: MusicQueue[] = $currentSessionQueue?.queues;
-		queues.splice(index, 1);
+		if (queues.length > 0) {
+			queues.splice(index, 1);
+		}
 		await supabase.from('session_queue').update({ queues }).eq('id', $currentSessionQueue?.id);
 	}
 </script>
 
 <!-- Each Queues -->
-<div class="w-full grid place-items-center">
+<div class="w-full grid place-items-center overflow-x-hidden">
 	{#each queues as queue, i}
-		<div class="rounded-2xl overflow-hidden relative cursor-pointer my-8">
-			<div class="gradient-hover-effect" />
-			<img
-				bind:this={imgRef}
-				src={queue?.track_image_url}
-				width="400"
-				height="400"
-				alt={`${queue.track_name} cover`}
-				on:error={handleImageError}
-				class="object-cover rounded-2xl absolute z-0"
-			/>
-			<div class="absolute z-20 p-2 bottom-0">
-				<p class="leading-tight">
-					{millisecondToMinuteSeconds(queue.duration_ms ?? 0)}
-				</p>
-				<p
-					class={`font-medium leading-tight ${
-						queue?.track_name.length > 20 ? 'text-lg' : 'text-xl'
-					}`}
+		<div
+			class="flex flex-row items-center w-full relative cursor-pointer h-20 hover:bg-black duration-200 rounded-md"
+		>
+			<p class="mx-2">{i + 1}</p>
+			<div class="rounded-2xl p-2 overflow-hidden font-normal flex flex-row items-center w-full">
+				<img
+					bind:this={imgRef}
+					src={queue?.track_image_url}
+					width="60"
+					height="60"
+					alt={`${queue?.track_name} cover`}
+					on:error={handleImageError}
+					class="object-cover rounded-md mr-2"
+				/>
+				<div class="text-ellipsis w-[80%]">
+					<p class="text-sm">{queue.track_name}</p>
+					<p class="leading-tight text-dark-100 text-xs">
+						{queue?.artist}
+					</p>
+				</div>
+				<button
+					on:click={() => removeQueue(i)}
+					id="delete-btn"
+					class="absolute z-20 p-2 top-4 right-0 active:scale-125 duration-150"
 				>
-					{queue?.track_name}
-				</p>
-				<p class={`${queue?.artist.length > 20 ? 'text-xs' : 'text-sm'} leading-tight`}>
-					{queue?.artist}
-				</p>
+					<Icon icon="mdi:trash" class="w-6 h-6 text-dark-200 hover:scale-110 duration-200" />
+				</button>
 			</div>
-			<button
-				on:click={() => removeQueue(i)}
-				id="delete-btn"
-				class="absolute z-20 p-2 top-0 right-0 active:scale-125 duration-150"
-			>
-				<Icon icon="mdi:trash" class="w-6 h-6 text-dark-200 hover:scale-110 duration-200" />
-			</button>
 		</div>
 	{/each}
 	<Tooltip triggeredBy="[id=delete-btn]" placement="right">Remove Queue</Tooltip>
 	<!-- Each Queues -->
 </div>
-
-<style>
-	.gradient-hover-effect {
-		width: 100%;
-		height: 100%;
-		background: linear-gradient(0deg, rgba(255, 255, 255, 0.06), transparent) rgba(0, 0, 0, 0.4);
-		transition: background-color 0.5s;
-		position: absolute;
-		z-index: 20;
-	}
-	.gradient-hover-effect:hover {
-		background: linear-gradient(0deg, rgba(0, 0, 0, 0.8), transparent) rgba(255, 255, 255, 0.1);
-	}
-</style>
