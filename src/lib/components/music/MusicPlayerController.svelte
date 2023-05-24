2f90f7641bf6086c9510d7c08d8a906c0879ce9a
ChuChuwiiii <chuwong35122@gmail.com>
2023-05-24 20:01:52 +0700

    ref: create components for music controller

--- src/lib/components/music/MusicPlayerController.svelte
+++ src/lib/components/music/MusicPlayerController.svelte
@@ -26,6 +26,11 @@
 	import { ioClient } from '$lib/socket/client';
 	import type { SessionBroadcastRequest } from '$lib/interfaces/session/socket.interface';
 	import { supabase } from '$lib/supabase/supabase';
+	import VolumeController from './player/VolumeController.svelte';
+	import TrackPreview from './player/TrackPreview.svelte';
+	import ControlButtons from './player/ControlButtons.svelte';
+	import PlayerTrackSlider from './player/LargePlayerTrackSlider.svelte';
+	import SmallPlayerTrackSlider from './player/SmallPlayerTrackSlider.svelte';
 
 	const socketConnection = ioClient.connect();
 
@@ -33,22 +38,13 @@
 	let SpotifyPlayer: Spotify.Player;
 	let timer: NodeJS.Timer;
 
-	let debounceVolumeTimer: NodeJS.Timer;
 	let volume = 50;
 	let debouncedVolume = 0;
 
 	let playingMs = 0;
 
-	async function debounceSetVolume() {
-		clearTimeout(debounceVolumeTimer);
-		debounceVolumeTimer = setTimeout(async () => {
-			debouncedVolume = volume / 100;
-			await SpotifyPlayer.setVolume(debouncedVolume);
-		}, 300);
-	}
-
 	function handleChangeSessionInfo() {
-		if ($currentSession) changeSessionPlayInfo($currentSession, $currentSessionRole, $playingInfo);
+		// if ($currentSession) changeSessionPlayInfo($currentSession, $currentSessionRole, $playingInfo);
 	}
 
 	// open broadcast modal, and play send track info
@@ -64,6 +60,7 @@
 		try {
 			// const broadcastPayload = createBroadcastPayload($currentSession, $playingInfo);
 			// socketConnection.emit('startSessionBroadcast', broadcastPayload);
+			console.log($spotifyAccessToken)
 
 			await playTrack(
 				$playingInfo,
@@ -92,8 +89,6 @@
 			console.log(e);
 		}
 	}
-
-	$: console.log($playingInfo);
 
 	async function togglePause() {
 		try {
@@ -151,7 +146,7 @@
 			});
 
 			SpotifyPlayer.on('player_state_changed', (val) => {
-				console.log(val);
+				// console.log(val);
 			});
 
 			// Put the connect() at the bottom most of player.on()
@@ -189,88 +184,14 @@
 <Modal bind:open={broadcastModal} size="xs" autoclose={false} class="modal-glass">
 	<SpotifyTrackBroadcastModal on:broadcast={togglePlay} />
 </Modal>
-<div class="relative w-full p-4 rounded-xl bg-dark-500 hover:bg-white/10 duration-200">
+<div class="relative w-full p-2 rounded-xl bg-dark-500 hover:bg-white/10 duration-200">
 	<div class="flex flex-row items-center justify-between">
-		{#if $playingInfo && $playingInfo?.track_image_url}
-			<div class="flex flex-col lg:flex-row items-center text-center lg:text-start">
-				<img
-					src={$playingInfo?.track_image_url}
-					alt="Playing track cover"
-					class="w-16 h-16 rounded-full mr-4"
-				/>
-				<div class="w-32 overflow-hidden">
-					<p class="font-medium truncate w-28">{$playingInfo?.track_name ?? ''}</p>
-					<p class="text-xs truncate w-28 text-dark-200">{$playingInfo?.artist}</p>
-				</div>
-			</div>
-		{:else}
-			<div class="w-20 h-16" />
-		{/if}
+		<TrackPreview />
 		<div class="flex flex-col items-center">
-			{#if $currentSessionRole === 'admin'}
-				<div class="flex flex-row items-center mb-2">
-					<button>
-						<Icon
-							icon="material-symbols:skip-previous-rounded"
-							class="w-10 h-10 hover:scale-110 duration-100"
-						/>
-					</button>
-					<button on:click={$playingInfo?.is_playing ? togglePause : togglePlay}>
-						{#if $playingInfo && $playingInfo?.is_playing}
-							<Icon
-								icon="material-symbols:pause-circle-rounded"
-								class="w-10 h-10 hover:scale-110 duration-100"
-							/>
-						{:else}
-							<Icon
-								icon="material-symbols:play-circle-rounded"
-								class="w-10 h-10 hover:scale-110 duration-100"
-							/>
-						{/if}
-					</button>
-					<button>
-						<Icon
-							icon="material-symbols:skip-next-rounded"
-							class="w-10 h-10 hover:scale-110 duration-100"
-						/>
-					</button>
-				</div>
-			{/if}
-			{#if $playingInfo?.is_playing}
-				<div class="flex flex-row items-center w-full justify-between my-[-8px]">
-					<div class="text-xs hidden lg:block w-8">{millisecondToMinuteSeconds(playingMs)}</div>
-					<input
-						min={0}
-						bind:value={playingMs}
-						max={$playingInfo?.duration_ms ?? 1}
-						on:mouseup={handleFastForwardTrack}
-						type="range"
-						class="w-[500px] h-8 accent-primary-500 mx-2 hidden lg:block"
-					/>
-					<div class="text-xs hidden lg:block w-8">
-						{millisecondToMinuteSeconds($playingInfo?.duration_ms ?? 0)}
-					</div>
-				</div>
-			{/if}
+			<ControlButtons {togglePlay} {togglePause} />
+			<PlayerTrackSlider {playingMs} {handleFastForwardTrack} />
 		</div>
-		<div class="flex flex-row items-center">
-			{#if volume === 0}
-				<Icon icon="material-symbols:volume-mute-rounded" class="w-6 h-6" />
-			{:else if volume > 0 && volume < 60}
-				<Icon icon="material-symbols:volume-down-rounded" class="w-6 h-6" />
-			{:else if volume >= 60}
-				<Icon icon="material-symbols:volume-up-rounded" class="w-6 h-6" />
-			{/if}
-			<div class="flex flex-col items-center justify-around">
-				<input
-					bind:value={volume}
-					on:keyup={debounceSetVolume}
-					type="range"
-					id="volume-sider"
-					class="accent-white w-16 md:w-24"
-				/>
-			</div>
-		</div>
+		<VolumeController {volume} {debouncedVolume} {SpotifyPlayer} />
 		<div class="absolute right-4 bottom-2">
 			<Icon
 				id="connected-player"
@@ -282,22 +203,5 @@
 			>Connected to {SpotifyPlayer?._options?.name}</Tooltip
 		>
 	</div>
-	{#if $playingInfo?.is_playing === true}
-		<div class="grid place-items-center mt-4">
-			<div class="w-[300px] md:w-[560px] flex flex-row items-center justify-between">
-				<div class="text-xs lg:hidden block w-8">{millisecondToMinuteSeconds(playingMs)}</div>
-				<div class="text-xs lg:hidden block w-8">
-					{millisecondToMinuteSeconds($playingInfo?.duration_ms ?? 0)}
-				</div>
-			</div>
-			<input
-				bind:value={playingMs}
-				min={0}
-				max={$playingInfo?.duration_ms ?? 1}
-				on:mouseup={handleFastForwardTrack}
-				type="range"
-				class="lg:hidden w-[300px] md:w-[560px] h-8 accent-primary-500 mx-2 block"
-			/>
-		</div>
-	{/if}
+<SmallPlayerTrackSlider {playingMs} {handleFastForwardTrack} />
 </div>
