47945cac167b09572ba22a888ae73ff7cef9d147
ChuChuwiiii <chuwong35122@gmail.com>
2023-06-04 13:27:56 +0700

    fix: json.parse error during Spotify play response

--- src/routes/api/spotify/playback/play/+server.ts
+++ src/routes/api/spotify/playback/play/+server.ts
@@ -1,29 +1,35 @@
-import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { PUBLIC_SPOTIFY_BASE_URL } from '$env/static/public';
 import { getBearerToken } from '$lib/spotify/spotify';
 
 // Play each Spotify track base on its position (ms)
-// uwis is in [spotify:track, ...] format
+// uris is in [spotify:track, ...] format
 export const POST: RequestHandler = async ({ fetch, request }) => {
 	const req = request.clone();
 	const _req = await req.json();
 	const { access_token, device_id, uris, position_ms } = _req;
+	// console.log(getBearerToken(access_token));
 
-	const res = await fetch(`${PUBLIC_SPOTIFY_BASE_URL}/me/player/play?device_id=${device_id}`, {
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: getBearerToken(access_token ?? '')
-		},
-		method: 'PUT',
-		body: JSON.stringify({
-			uris: uris,
-			position_ms: position_ms
-		})
-	});
+	let status = 202;
+	try {
+		const res = await fetch(`${PUBLIC_SPOTIFY_BASE_URL}/me/player/play?device_id=${device_id}`, {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: getBearerToken(access_token ?? '')
+			},
+			method: 'PUT',
+			body: JSON.stringify({
+				uris: uris,
+				position_ms: position_ms
+			})
+		});
 
-	const response = await res.json();
-	return json({
-		status: response.status
-	});
+		status = res.status;
+		return new Response(JSON.stringify({ ok: true }));
+	} catch (e) {
+		if (status === 202) {
+			return new Response(JSON.stringify({ ok: true }));
+		}
+		return new Response(JSON.stringify({ ok: false }));
+	}
 };
