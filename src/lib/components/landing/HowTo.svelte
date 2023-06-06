7c23135f28f14ffca39d1bcd5b7c7777cedfbd40
ChuChuwiiii <chuwong35122@gmail.com>
2023-06-06 13:01:22 +0700

    feat: create landing page with how-to-use steps

---
+++ src/lib/components/landing/HowTo.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	export let step = '';
+	export let description = '';
+	export let icon = '';
+	export let hasNext = true;
+</script>
+
+<div class="p-2 w-48 my-2 rounded-lg relative bg-black/20">
+	<p>{step}</p>
+	<div class="w-full h-full grid place-items-center">
+		<Icon {icon} class="w-12 h-12 mb-2" />
+		<p class="text-lg">{description}</p>
+	</div>
+
+	{#if hasNext}
+		<Icon
+			icon="icon-park-solid:right-c"
+			class="hidden lg:block w-12 h-12 text-black/40 absolute right-[-20px] top-[30%]"
+		/>
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,44 @@
+<script lang="ts">
+	import HowToSteps from '../lib/components/landing/HowToSteps.svelte';
+
+	let mousePos = { x: 0, y: 0 };
+	function handleMouseMove(e: MouseEvent) {
+		mousePos.x = e.pageX;
+		mousePos.y = e.pageY;
+	}
+</script>
+
 <svelte:head>
 	<title>Welcome to Chimefy</title>
 </svelte:head>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div on:mousemove={handleMouseMove} class='w-full'>
+<section>
+	<div class="w-full h-[600px] p-8 relative overflow-hidden">
+		<div>
+			<h1 class="text-6xl font-extrabold ml-2">Welcome to Chimefy</h1>
+		</div>
+		<h2 class="ml-4">
+			A place to sync <a
+				target="_blank"
+				rel="noreferrer"
+				href="https://www.spotify.com/"
+				class="text-primary underline underline-offset-1">Spotify music</a
+			> with your friends!
+		</h2>
+		<div class="absolute bottom-0 right-0">
+			<img
+				src="/images/landing/friends-listening.png"
+				alt="Friends listening to music"
+				class="w-[550px] h-[550px] md:w-[720px] md:h-[720px] lg:w-[800px] lg:h-[800px] relative z-50"
+			/>
+			<div
+				class="absolute z-10 right-0 top-[240px] bg-white/40 transform -skew-y-[20deg] skew-x-[10deg] rounded-lg w-[500px] h-[440px]  lg:w-[600px] lg:h-[500px]"
+			/>
+		</div>
+	</div>
+</section>
+<section class="h-[400px] w-full">
+	<HowToSteps mousePosX={mousePos.x} mousePosY={mousePos.y} />
+</section>
+</div>
