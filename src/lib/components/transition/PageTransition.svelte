87d70465c7fd26e71eb429714bbb5733b956913b
ChuChuwiiii <chuwong35122@gmail.com>
2023-06-08 09:42:32 +0700

    feat: add page transition when url changes

---
+++ src/lib/components/transition/PageTransition.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { fly, fade } from "svelte/transition";
+  export let url = "";
+  const pageTransitionDuration = 250;
+</script>
+
+{#key url}
+  <div in:fade={{  duration: pageTransitionDuration, delay: pageTransitionDuration }}
+       out:fly={{ x: 5, duration: pageTransitionDuration }} class='w-full'>
+    <slot />
+  </div>
+{/key}
