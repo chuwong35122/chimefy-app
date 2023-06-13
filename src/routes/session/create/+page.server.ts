41dca7fc600589bdb99b6262bda2364a08250e68
ChuChuwiiii <chuwong35122@gmail.com>
2023-06-13 14:15:36 +0700

    fix: server-side redirect error

--- src/routes/session/create/+page.server.ts
+++ src/routes/session/create/+page.server.ts
@@ -38,7 +38,6 @@ export const actions = {
 			queues: null
 		};
 
-		try {
 			const { data } = await event.locals.supabase.from('session').insert(payload).select();
 
 			if (data && data[0] && data[0]?.id && data[0]?.uuid != null) {
@@ -61,13 +60,7 @@ export const actions = {
 					.insert({ members: [], session_uuid: data[0]?.uuid, session_id: data[0]?.id })
 					.select();
 
-				throw redirect(303, `/session/${data[0].uuid}`);
+				throw redirect(302, `/session/${data[0].uuid}`);
 			}
-		} catch (e) {
-			const err = e as PostgrestError;
-			return fail(400, {
-				error: err
-			});
-		}
 	}
 };
