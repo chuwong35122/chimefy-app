9bf94afb25c852aceff65f72cb97f7b912b426db
ChuChuwiiii <chuwong35122@gmail.com>
2023-06-15 20:03:46 +0700

    feat: create error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { page } from '$app/stores';
+	import PrimaryButtonWrapper from '../lib/components/buttons/PrimaryButtonWrapper.svelte';
+import { base } from '$app/paths'
+	import { goto } from '$app/navigation';
+
+let previousPage = base || '/';
+
+function goBack() {
+  goto(previousPage)
+}
+
+</script>
+
+<div class="mt-60 w-screen grid place-items-center">
+	<div class="grid place-items-center w-full">
+		<h1 class="text-[100px] font-bold">{$page?.status}</h1>
+		<p class="text-3xl">{$page?.error?.message}</p>
+		<div class="mt-4 grid place-items-center">
+			<p>Sorry, something just went wrong...</p>
+			<button on:click={goBack} class='mt-4'>
+				<PrimaryButtonWrapper>Go Back</PrimaryButtonWrapper>
+			</button>
+		</div>
+	</div>
+</div>
