17a68a3d7f44ad233fbd14db0c8293f902feb0a3
ChuChuwiiii <chuwong35122@gmail.com>
2023-06-29 21:29:19 +0700

    feat: delete session modal

--- src/lib/components/music/SessionInfo.svelte
+++ src/lib/components/music/SessionInfo.svelte
@@ -1,21 +1,54 @@
 <script lang="ts">
 	import { toastValue } from '$lib/notification/toast';
-	import { Tooltip } from 'flowbite-svelte';
+	import { Button, Modal, Tooltip } from 'flowbite-svelte';
 	import { currentSession } from '$lib/session/session';
 	import Icon from '@iconify/svelte';
 	import Chip from '../UI/Chip.svelte';
+	import { supabase } from '$lib/supabase/supabase';
+	import { goto } from '$app/navigation';
 
 	export let sessionId = '';
+
+	let showDeleteModel = false;
 
 	function onCopySessionId() {
 		toastValue.set({ message: "Session's ID copied!", type: 'info' });
 		navigator.clipboard.writeText(sessionId);
 	}
+
+	function handleOpenDeleteModal() {
+		showDeleteModel = true;
+	}
+
+	async function handleDeleteSession() {
+		if (!$currentSession?.id) return;
+		const { error } = await supabase.from('session').delete().eq('id', $currentSession.id);
+		goto('/session');
+		if (error?.code) {
+			toastValue.set({ message: 'Cannot delete session due to an error.', type: 'error' });
+		}
+	}
 </script>
 
-<Tooltip triggeredBy="[id=copy-id-btn]" placement="left">Copy Session's ID</Tooltip>
+<Modal bind:open={showDeleteModel} size="xs" autoclose class="!bg-black">
+	<div class="text-center">
+		<div class="w-full grid place-items-center">
+			<Icon icon="fluent-emoji:thinking-face" class="w-20 h-20" />
+		</div>
+		<h3 class="text-white my-6">Are you sure you want to delete this session?</h3>
+		<Button
+			on:click={handleDeleteSession}
+			color="red"
+			class="mr-2 hover:scale-105 duration-150 w-40">Delete it!</Button
+		>
+		<Button color="alternative" class="hover:scale-105 duration-150 w-40">Nevermind</Button>
+	</div>
+</Modal>
+
+<Tooltip triggeredBy="[id=delete-id-btn]" placement="left">Delete Session</Tooltip>
+<Tooltip triggeredBy="[id=copy-id-btn]" placement="right">Copy Session's ID</Tooltip>
 {#if $currentSession?.is_private}
-	<Tooltip triggeredBy="[id='isPrivate-icon']"
+	<Tooltip triggeredBy="[id='isPrivate-icon']" placement="right"
 		>{$currentSession?.is_private
 			? 'This session is a private session'
 			: 'This session is a public session'}</Tooltip
@@ -47,12 +80,26 @@
 			<Chip label={$currentSession?.type} />
 		</div>
 	</div>
-	<button id="copy-id-btn" on:click={onCopySessionId} class="hover:scale-[1.1] duration-200">
-		<Icon
-			icon="material-symbols:content-copy"
-			width={30}
-			height={30}
-			class="text-gray-500 hover:text-gray-300 duration-200"
-		/>
-	</button>
+	<div class="flex flex-row items-center">
+		<button
+			id="delete-id-btn"
+			on:click={handleOpenDeleteModal}
+			class="hover:scale-[1.1] duration-200 mr-4"
+		>
+			<Icon
+				icon="mdi:delete"
+				width={30}
+				height={30}
+				class="text-red-800 hover:text-red-700 duration-200"
+			/>
+		</button>
+		<button id="copy-id-btn" on:click={onCopySessionId} class="hover:scale-[1.1] duration-200">
+			<Icon
+				icon="material-symbols:content-copy"
+				width={30}
+				height={30}
+				class="text-gray-500 hover:text-gray-300 duration-200"
+			/>
+		</button>
+	</div>
 </div>
