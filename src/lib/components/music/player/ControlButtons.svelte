c3cd3b6f6caf3323ed424d8d08d2cc16e1a7ed1a
ChuChuwiiii <chuwong35122@gmail.com>
2023-07-05 23:48:29 +0700

    feat: show disabled button to session members

--- src/lib/components/music/player/ControlButtons.svelte
+++ src/lib/components/music/player/ControlButtons.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { currentSessionRole, isPlayingStatus } from '$lib/session/session';
 	import Icon from '@iconify/svelte';
+	import { Tooltip } from 'flowbite-svelte';
 
 	export let onBroadcastSignal: (playing: boolean) => void;
 	export let onForwardTrack: () => void;
@@ -15,34 +16,45 @@
 	}
 </script>
 
-<div>
-	{#if $currentSessionRole === 'admin'}
-		<div class="flex flex-row items-center mb-2">
-			<button on:click={onBackwardTrack}>
-				<Icon
-					icon="material-symbols:skip-previous-rounded"
-					class="w-10 h-10 hover:scale-110 duration-100"
-				/>
-			</button>
-			<button on:click={$isPlayingStatus ? togglePause : togglePlay}>
-				{#if $isPlayingStatus}
-					<Icon
-						icon="material-symbols:pause-circle-rounded"
-						class="w-10 h-10 hover:scale-110 duration-100"
-					/>
-				{:else}
-					<Icon
-						icon="material-symbols:play-circle-rounded"
-						class="w-10 h-10 hover:scale-110 duration-100"
-					/>
-				{/if}
-			</button>
-			<button on:click={onForwardTrack}>
-				<Icon
-					icon="material-symbols:skip-next-rounded"
-					class="w-10 h-10 hover:scale-110 duration-100"
-				/>
-			</button>
-		</div>
-	{/if}
+{#if $currentSessionRole === 'member'}
+	<Tooltip triggeredBy="#controller-section">Only admin controls the player</Tooltip>
+{/if}
+<div id="controller-section" class="flex flex-row items-center mb-2">
+	<button
+		disabled={$currentSessionRole === 'member'}
+		on:click={onBackwardTrack}
+		class="disabled:text-dark-400"
+	>
+		<Icon
+			icon="material-symbols:skip-previous-rounded"
+			class="w-10 h-10 hover:scale-110 duration-100"
+		/>
+	</button>
+	<button
+		disabled={$currentSessionRole === 'member'}
+		on:click={$isPlayingStatus ? togglePause : togglePlay}
+		class="disabled:text-dark-400"
+	>
+		{#if $isPlayingStatus}
+			<Icon
+				icon="material-symbols:pause-circle-rounded"
+				class="w-10 h-10 hover:scale-110 duration-100"
+			/>
+		{:else}
+			<Icon
+				icon="material-symbols:play-circle-rounded"
+				class="w-10 h-10 hover:scale-110 duration-100"
+			/>
+		{/if}
+	</button>
+	<button
+		disabled={$currentSessionRole === 'member'}
+		on:click={onForwardTrack}
+		class="disabled:text-dark-400"
+	>
+		<Icon
+			icon="material-symbols:skip-next-rounded"
+			class="w-10 h-10 hover:scale-110 duration-100"
+		/>
+	</button>
 </div>
