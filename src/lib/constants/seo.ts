ea8dbcecc75c72aadf9a725d35bebde0cd5f676d
ChuChuwiiii <chuwong35122@gmail.com>
2023-07-07 22:27:21 +0700

    ref: change image format to AVIF

--- src/lib/constants/seo.ts
+++ src/lib/constants/seo.ts
@@ -2,7 +2,7 @@ import { PUBLIC_SITE_URL } from '$env/static/public';
 
 const seo = {
 	twitterCard: 'summary_large_image',
-	image: '/images/seo/music-along.jpg',
+	image: '/images/seo/music-along.avif',
 	imageAlt: 'Listen to music together with friends',
 
 	appTitle: 'Listen to Spotify music together with friends',
@@ -17,7 +17,7 @@ const seo = {
 	sessionInvite: {
 		// title, url, image alt is dynamic
 		description: 'Your friend has invited you to join Chimefy session. Login and listen together!',
-		image: '/images/seo/enter-session.png'
+		image: '/images/seo/enter-session.avif'
 	}
 };
 
