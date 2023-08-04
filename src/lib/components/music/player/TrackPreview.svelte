01ca3d901735101477f48d1f90733c4f5f2e9166
ChuChuwiiii <chuwong35122@gmail.com>
2023-08-04 20:44:29 +0700

    feat: remove rounded Spotify's artwork

--- src/lib/components/music/player/TrackPreview.svelte
+++ src/lib/components/music/player/TrackPreview.svelte
@@ -5,11 +5,7 @@
 <div class="h-20">
 	{#if $playingInfo && $playingInfo?.track_image_url}
 		<div class="flex flex-col md:flex-row items-center text-start lg:text-start">
-			<img
-				src={$playingInfo?.track_image_url}
-				alt="Playing track cover"
-				class="w-20 h-20 mr-4"
-			/>
+			<img src={$playingInfo?.track_image_url} alt="Playing track cover" class="w-20 h-20 mr-4" />
 			<div class="w-32 overflow-hidden invisible md:visible">
 				<p class="text-sm truncate w-28 font-medium">{$playingInfo?.track_name ?? ''}</p>
 				<p class="text-xs truncate w-28 text-dark-200">{$playingInfo?.artist}</p>
