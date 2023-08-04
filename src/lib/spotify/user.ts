22693f69702afd48ae1b1a302ad9ab376f000641
ChuChuwiiii <chuwong35122@gmail.com>
2023-08-04 22:00:19 +0700

    fix: update Spotify auth scopes

--- src/lib/spotify/user.ts
+++ src/lib/spotify/user.ts
@@ -4,18 +4,8 @@ import type { PrivateUser, AccessToken } from 'spotify-types';
 export const SPOTIFY_AUTH_SCOPES = [
 	'user-read-playback-state',
 	'user-modify-playback-state',
-	'user-read-currently-playing',
 	'app-remote-control',
 	'streaming',
-	'playlist-read-private',
-	'playlist-read-collaborative',
-	'playlist-modify-private',
-	'playlist-modify-public',
-	'user-read-playback-position',
-	'user-top-read',
-	'user-read-recently-played',
-	'user-library-modify',
-	'user-library-read',
 	'user-read-email',
 	'user-read-private'
 ];
