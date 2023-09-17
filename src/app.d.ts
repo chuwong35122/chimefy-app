5cbdc5d8ca53e5971009964684579c5af24167da
ChuChuwiiii <chuwong35122@gmail.com>
2023-09-17 12:19:16 +0700

    feat: add database types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,4 +1,5 @@
 // See https://kit.svelte.dev/docs/types#app
+import type { Database } from '$interfaces/database/database.interface';
 import { SupabaseClient, type Session } from '@supabase/supabase-js';
 
 // for information about these interfaces
@@ -6,7 +7,7 @@ declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			supabase: SupabaseClient;
+			supabase: SupabaseClient<Database>;
 			getSession(): Promise<Session | null>;
 			pathName: string;
 		}
