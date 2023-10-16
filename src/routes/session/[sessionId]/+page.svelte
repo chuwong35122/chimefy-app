35bf59589988bd91f30fa45d728d9b3458625403
ChuChuwiiii <chuwong35122@gmail.com>
2023-10-16 20:08:30 +0700

    feat: update load function to be space instead of session

--- src/routes/session/[sessionId]/+page.svelte
+++ src/routes/session/[sessionId]/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import type { MusicSession } from '$interfaces/session/session.interface';
 	import { onMount } from 'svelte';
+	import { currentSession, currentSessionRole, currentSessionQueue, spaceStore, spaceRoleStore } from '$stores/session';
 	import TrackSearchTab from '$components/music/TrackSearchTab.svelte';
-	import { currentSession, currentSessionRole, currentSessionQueue } from '$stores/session';
-	import TrackQueueList from '$components/music/TrackQueueList.svelte';
-	import MusicPlayerController from '$components/music/MusicPlayerController.svelte';
-	import SessionMembers from '$components/music/members/SessionMembers.svelte';
+	// import TrackQueueList from '$components/music/TrackQueueList.svelte';
+	// import MusicPlayerController from '$components/music/MusicPlayerController.svelte';
+	// import SessionMembers from '$components/music/members/SessionMembers.svelte';
 	import SessionInfo from '$components/music/SessionInfo.svelte';
-	import { supabase } from '$supabase/supabase';
 	import { userStore } from '$stores/auth/user';
 	import type { MusicSessionQueue } from '$interfaces/session/queue.interface';
 	import seo from '$constants/seo';
@@ -15,8 +14,10 @@
 	import { Modal } from 'flowbite-svelte';
 	import SessionHelpModal from '$components/modals/SessionHelpModal.svelte';
 
-	export let data: { session: MusicSession; queues: MusicSessionQueue; url: string };
-	let { session, queues, url } = data;
+	export let data
+	let { space, url, supabase } = data;
+
+	$: spaceStore.set(space)
 
 	let openTutorialModal = false;
 
@@ -28,29 +29,32 @@
 		currentSessionRole.set(
 			$currentSession?.created_by.toString() === $userStore?.id.toString() ? 'admin' : 'member'
 		);
+		spaceRoleStore.set(
+			$currentSession?.created_by.toString() === $userStore?.id.toString() ? 'admin' : 'member'
+		);
 	}
 
-	onMount(() => {
-		currentSession.set(session as MusicSession);
-		sessionId = session?.uuid ?? '';
-		currentSessionQueue.set(queues as MusicSessionQueue);
+	// onMount(() => {
+	// 	currentSession.set(session as MusicSession);
+	// 	sessionId = session?.uuid ?? '';
+	// 	currentSessionQueue.set(queues as MusicSessionQueue);
 
-		const storedVolume = localStorage.getItem('player_volume');
-		if (storedVolume) {
-			playerVolume = Number(storedVolume);
-		}
+	// 	const storedVolume = localStorage.getItem('player_volume');
+	// 	if (storedVolume) {
+	// 		playerVolume = Number(storedVolume);
+	// 	}
 
-		channel = supabase
-			.channel(`session_queue_listener_${sessionId}`)
-			.on(
-				'postgres_changes',
-				{ event: '*', schema: 'public', table: 'session_queue' },
-				(payload) => {
-					currentSessionQueue.set(payload.new as MusicSessionQueue);
-				}
-			)
-			.subscribe();
-	});
+	// 	channel = supabase
+	// 		.channel(`session_queue_listener_${sessionId}`)
+	// 		.on(
+	// 			'postgres_changes',
+	// 			{ event: '*', schema: 'public', table: 'session_queue' },
+	// 			(payload) => {
+	// 				currentSessionQueue.set(payload.new as MusicSessionQueue);
+	// 			}
+	// 		)
+	// 		.subscribe();
+	// });
 </script>
 
 <svelte:head>
@@ -63,20 +67,20 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 	<!-- Open Graph Meta Tags -->
-	<meta property="og:title" content={`Join ${session?.name}`} />
+	<meta property="og:title" content={`Join ${space?.name}`} />
 	<meta property="og:description" content={seo.appDescription} />
 	<meta property="og:image" content={seo.sessionInvite.image} />
 	<meta property="og:url" content={url} />
 	<meta property="og:type" content="website" />
 
 	<!-- Twitter Meta Tags -->
-	<meta name="twitter:title" content={`You have been invited to Chimefy: ${session?.name}`} />
+	<meta name="twitter:title" content={`You have been invited to Chimefy: ${space?.name}`} />
 	<meta name="twitter:description" content={seo.sessionInvite.description} />
 	<meta name="twitter:card" content={seo.twitterCard} />
 	<meta name="twitter:image" content={seo.sessionInvite.image} />
-	<meta name="twitter:image:alt" content={`Join ${session?.name}`} />
+	<meta name="twitter:image:alt" content={`Join ${space?.name}`} />
 
-	<title>{session?.name ? `Listening to ${session?.name}` : 'Loading...'}</title>
+	<title>{space?.name ? `Listening to ${space?.name}` : 'Loading...'}</title>
 </svelte:head>
 
 <Modal bind:open={openTutorialModal} size="lg" class="modal-glass">
@@ -85,18 +89,18 @@
 
 <div class="w-[400px] md:w-[640px] lg:w-[1000px] lg:h-[640px]">
 	<div class="mt-8">
-		<SessionInfo {sessionId} />
+		<SessionInfo {supabase} />
 	</div>
 	{#if $userStore}
 		<div class="bg-[rgba(255,255,255,0.05)] rounded-xl">
 			<div class="flex flex-col lg:flex-row w-full">
-				<TrackSearchTab />
+				<TrackSearchTab {supabase} />
 				<div class="w-full">
 					<div class="w-full grid place-items-center my-2 mt-8">
 						<p class="text-2xl font-semibold">Queues</p>
 					</div>
 					<div class="w-full h-[592px] overflow-y-auto">
-						<TrackQueueList />
+						<!-- <TrackQueueList /> -->
 					</div>
 				</div>
 			</div>
@@ -109,8 +113,8 @@
 		</div>
 		{#if $currentSession && $currentSession?.id}
 			<div class="h-24">
-				<MusicPlayerController sessionId={$currentSession?.id} volume={playerVolume} />
-				<SessionMembers sessionId={$currentSession?.id} />
+				<!-- <MusicPlayerController sessionId={$currentSession?.id} volume={playerVolume} /> -->
+				<!-- <SessionMembers sessionId={$currentSession?.id} /> -->
 			</div>
 		{/if}
 	{/if}
