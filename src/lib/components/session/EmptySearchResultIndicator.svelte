808c5b60ba3cbb2236d3d7fd3ee13672a1d34301
ChuChuwiiii <chuwong35122@gmail.com>
2023-10-21 21:47:12 +0700

    fix: typo

--- src/lib/components/session/EmptySearchResultIndicator.svelte
+++ src/lib/components/session/EmptySearchResultIndicator.svelte
@@ -2,5 +2,5 @@
 	<div class="invert">
 		<img src="/images/placeholders/sleeping.png" alt="black cat sleeping by Marz Gallery" class="w-20 h-20" />
 	</div>
-  <p class='text-xs'>The black cat is taking up all these spaces as there is no result...</p>
+  <p class='text-xs'>The white cat is taking up all these spaces as there is no result...</p>
 </div>
