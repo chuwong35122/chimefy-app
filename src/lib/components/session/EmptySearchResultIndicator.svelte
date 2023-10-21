452ca57418b64419e73080014aa2456e1ebf4148
ChuChuwiiii <chuwong35122@gmail.com>
2023-10-21 11:09:45 +0700

    feat: add empty music session indicator

---
+++ src/lib/components/session/EmptySearchResultIndicator.svelte
@@ -0,0 +1,6 @@
+<div class='w-full grid place-items-center opacity-70'>
+	<div class="invert">
+		<img src="/images/placeholders/sleeping.png" alt="black cat sleeping by Marz Gallery" class="w-20 h-20" />
+	</div>
+  <p class='text-xs'>The black cat is taking up all these spaces as there is no result...</p>
+</div>
