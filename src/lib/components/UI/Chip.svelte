3d934a8d837753ffa76dbf8b067a29a83a6ae1e5
ChuChuwiiii <chuwong35122@gmail.com>
2023-10-23 17:42:46 +0700

    feat: change space's info style

--- src/lib/components/UI/Chip.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let label = '';
-</script>
-
-<div
-	class="px-1 bg-dark-200 rounded-full hover:bg-white duration-200 grid place-items-center cursor-default"
->
-	<span class="font-medium text-xs text-black">{label}</span>
-</div>
