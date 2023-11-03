2e1c6d279af21a3b9c7d38efe68c36a3bd8667b3
ChuChuwiiii <chuwong35122@gmail.com>
2023-11-03 19:45:05 +0700

    feat: update form UI with frosted glass background

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -52,83 +52,87 @@
 	<title>Create a space</title>
 </svelte:head>
 
-<div class="p-10 bg-white/5 rounded-2xl duration-150 hover:bg-white/10 w-[500px] mt-20">
-	{#if $devModeStore}
-		<SuperDebug data={$form} />
-	{/if}
+<div class="w-full grid place-items-center">
+	<div
+		class="p-10 bg-glass rounded-2xl duration-150 hover:bg-white/[0.08] w-[500px] mt-20"
+	>
+		{#if $devModeStore}
+			<SuperDebug data={$form} />
+		{/if}
 
-	<Tooltip triggeredBy="[id=private-info]" placement="right">
-		Private space will not be shown on the search section.
-	</Tooltip>
-	<Tooltip triggeredBy="[id=allow-queue-info]" placement="right">
-		Do you want to allow your members to add track(s) to the queue?
-	</Tooltip>
+		<Tooltip triggeredBy="[id=private-info]" placement="right">
+			Private space will not be shown on the search section.
+		</Tooltip>
+		<Tooltip triggeredBy="[id=allow-queue-info]" placement="right">
+			Do you want to allow your members to add track(s) to the queue?
+		</Tooltip>
 
-	<form class="flex flex-col space-y-4" method="POST">
-		<h3 class="text-2xl font-semibold text-primary-500 p-0">Create a Space</h3>
-		<Label class="space-y-2">
-			<span class="text-white">Your space name</span>
-			<Input
-				bind:value={$form.name}
-				name="name"
-				type="text"
-				placeholder="Space Name"
-				required
-				color="green"
-			/>
-			{#if $errors?.name && $errors.name[0]}
-				<ErrorMessage message={$errors?.name[0]} />
-			{/if}
-		</Label>
-		<Label>
-			<span class="text-white">Music Type</span>
-			<Select
-				name="type"
-				underline
-				bind:value={$form.type}
-				placeholder="Select music type"
-				items={musicSpaceTypes}
-				class="!text-white mb-2"
-			/>
-			{#if $errors?.type}
-				<ErrorMessage message={$errors?.type[0]} />
-			{/if}
-		</Label>
-		<div class="flex flex-row items-center">
-			<input
-				type="checkbox"
-				name="is_private"
-				color="green"
-				bind:checked={$form.is_private}
-				class="text-primary mr-2 rounded-sm"
-			/>
-			<p>Set this space private?</p>
-			<Icon
-				icon="material-symbols:info"
-				id="private-info"
-				class="text-gray-400 hover:text-gray-200 duration-150 ml-2"
-			/>
-		</div>
-		<div class="flex flex-row items-center">
-			<input
-				type="checkbox"
-				name="allow_member_queue"
-				color="green"
-				bind:checked={$form.allow_member_queue}
-				class="text-primary mr-2 rounded-sm"
-			/>
-			<p>Allow member to set queue?</p>
-			<Icon
-				icon="material-symbols:info"
-				id="allow-queue-info"
-				class="text-gray-400 hover:text-gray-200 duration-150 ml-2"
-			/>
-		</div>
-		<GradientButton type="submit" disabled={$delayed} color="green" pill
-			>Create Space</GradientButton
-		>
-		<Button type="button" on:click={() => goto('/space')} color="alternative" pill class="w-full"
-			>Go Back</Button
-		>
-	</form>
+		<form class="flex flex-col space-y-4" method="POST">
+			<h3 class="text-2xl font-semibold text-primary-500 p-0">Create a Space</h3>
+			<Label class="space-y-2">
+				<span class="text-white">Your space name</span>
+				<Input
+					bind:value={$form.name}
+					name="name"
+					type="text"
+					placeholder="Space Name"
+					required
+					color="green"
+				/>
+				{#if $errors?.name && $errors.name[0]}
+					<ErrorMessage message={$errors?.name[0]} />
+				{/if}
+			</Label>
+			<Label>
+				<span class="text-white">Music Type</span>
+				<Select
+					name="type"
+					underline
+					bind:value={$form.type}
+					placeholder="Select music type"
+					items={musicSpaceTypes}
+					class="!text-white mb-2"
+				/>
+				{#if $errors?.type}
+					<ErrorMessage message={$errors?.type[0]} />
+				{/if}
+			</Label>
+			<div class="flex flex-row items-center">
+				<input
+					type="checkbox"
+					name="is_private"
+					color="green"
+					bind:checked={$form.is_private}
+					class="text-primary mr-2 rounded-sm"
+				/>
+				<p>Set this space private?</p>
+				<Icon
+					icon="material-symbols:info"
+					id="private-info"
+					class="text-gray-400 hover:text-gray-200 duration-150 ml-2"
+				/>
+			</div>
+			<div class="flex flex-row items-center">
+				<input
+					type="checkbox"
+					name="allow_member_queue"
+					color="green"
+					bind:checked={$form.allow_member_queue}
+					class="text-primary mr-2 rounded-sm"
+				/>
+				<p>Allow member to set queue?</p>
+				<Icon
+					icon="material-symbols:info"
+					id="allow-queue-info"
+					class="text-gray-400 hover:text-gray-200 duration-150 ml-2"
+				/>
+			</div>
+			<GradientButton type="submit" disabled={$delayed} color="green" pill
+				>Create Space</GradientButton
+			>
+			<Button type="button" on:click={() => goto('/space')} color="alternative" pill class="w-full"
+				>Go Back</Button
+			>
+		</form>
+	</div>
 </div>
