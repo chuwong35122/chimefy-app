76ea66c81d6c83ee4618c21afec134312ad6043f
ChuChuwiiii <chuwong35122@gmail.com>
2023-11-08 20:05:27 +0700

    feat: add margin

--- src/lib/components/space/track/TrackSearchTab.svelte
+++ src/lib/components/space/track/TrackSearchTab.svelte
@@ -89,7 +89,7 @@
 				</div>
 			</TabItem>
 			<TabItem title="Playlists" on:click={() => (type = 'playlist')}>
-				<p class="text-xs text-gray-400">Maximum of 30 tracks can be added to the queue</p>
+				<p class="text-xs text-gray-400 mt-2">Maximum of 30 tracks can be added to the queue</p>
 				<div class="w-full h-[420px] overflow-y-auto overflow-x-hidden">
 					{#if loading}
 						<div class="w-full h-full grid place-items-center">
