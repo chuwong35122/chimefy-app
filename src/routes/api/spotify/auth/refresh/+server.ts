3b3ec796f21220dc0a8e8b899070345497ccb6e1
ChuChuwiiii <chuwong35122@gmail.com>
2023-11-12 21:21:59 +0700

    ref: remove unnecessary fields

--- src/routes/api/spotify/auth/refresh/+server.ts
+++ src/routes/api/spotify/auth/refresh/+server.ts
@@ -8,17 +8,20 @@ import { PRIVATE_SPOTIFY_CLIENT_SECRET } from '$env/static/private';
 // Refresh Spotify's Access Token
 export const POST: RequestHandler = async ({ fetch, request }) => {
 	const { refresh_token } = await request.json();
-	const authHeader =
-		'Basic ' +
-		Buffer.from(PUBLIC_SPOTIFY_CLIENT_ID + ':' + PRIVATE_SPOTIFY_CLIENT_SECRET).toString('base64');
+
 	const res = await fetch(`https://accounts.spotify.com/api/token`, {
-		body: qs.stringify({ grant_type: 'refresh_token', refresh_token: refresh_token }),
 		method: 'POST',
 		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-			Authorization: authHeader
-		}
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		body: new URLSearchParams({
+			grant_type: 'refresh_token',
+			refresh_token,
+			client_id: PUBLIC_SPOTIFY_CLIENT_ID,
+			client_secret: PRIVATE_SPOTIFY_CLIENT_SECRET
+		})
 	});
+
 	const authToken = (await res.json()) as AuthTokens;
 	return json(authToken);
 };
