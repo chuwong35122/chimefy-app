0e52b69b67c6f4da2e8664d7d9cbe2c4105a390e
ChuChuwiiii <chuwong35122@gmail.com>
2023-11-13 19:40:45 +0700

    fix: Spotify refresh token route

--- src/routes/api/spotify/auth/refresh/+server.ts
+++ src/routes/api/spotify/auth/refresh/+server.ts
@@ -6,7 +6,8 @@ import { PRIVATE_SPOTIFY_CLIENT_SECRET } from '$env/static/private';
 
 // Refresh Spotify's Access Token
 export const POST: RequestHandler = async ({ fetch, request }) => {
-	const { refresh_token } = await request.json();
+	const data = await request.formData();
+	const refreshToken = String(data.get('refresh_token'));
 
 	const res = await fetch(`https://accounts.spotify.com/api/token`, {
 		method: 'POST',
@@ -15,12 +16,11 @@ export const POST: RequestHandler = async ({ fetch, request }) => {
 		},
 		body: new URLSearchParams({
 			grant_type: 'refresh_token',
-			refresh_token,
+			refresh_token: refreshToken,
 			client_id: PUBLIC_SPOTIFY_CLIENT_ID,
 			client_secret: PRIVATE_SPOTIFY_CLIENT_SECRET
 		})
 	});
 
-	const authToken = (await res.json()) as AuthTokens;
-	return json(authToken);
+	return json((await res.json()) as AuthTokens);
 };
