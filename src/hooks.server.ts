7238751563ab0ca618b18efdc90eee5779716c8a
ChuChuwiiii <chuwong35122@gmail.com>
2023-11-16 20:41:31 +0700

    fix: handle invalid session or error when retrieve session

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,7 @@ import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/publi
 import { PUBLIC_NODE_ENV } from '$env/static/public';
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
 import { redirect, type Handle, type HandleServerError } from '@sveltejs/kit';
+import { invalidate } from '$app/navigation';
 
 const UNPROTECTED_ROUTES = ['/', '/contact', '/auth/spotify', '/auth/callback', '/auth/refresh'];
 
@@ -20,8 +21,14 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 	event.locals.getSession = async () => {
 		const {
-			data: { session }
+			data: { session },
+			error
 		} = await event.locals.supabase.auth.getSession();
+
+		if (!session || error) {
+			invalidate('supabase:auth');
+		}
+
 		return session;
 	};
 
