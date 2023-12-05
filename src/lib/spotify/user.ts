b62b4fb8ac31e753336676c50eea02aee6ad0a9b
ChuChuwiiii <chuwong35122@gmail.com>
2023-12-05 11:40:37 +0700

    fix: remove app-remote-control scope

--- src/lib/spotify/user.ts
+++ src/lib/spotify/user.ts
@@ -1,10 +1,8 @@
-import { appTokenStore } from '$stores/spotify/user';
-import type { PrivateUser, AccessToken } from 'spotify-types';
+import type { PrivateUser } from 'spotify-types';
 
 export const SPOTIFY_AUTH_SCOPES = [
 	'user-read-playback-state',
 	'user-modify-playback-state',
-	'app-remote-control',
 	'streaming',
 	'user-read-email',
 	'user-read-private'
